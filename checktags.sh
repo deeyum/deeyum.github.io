1336618e4efc22dc647903c86132223033472cae
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2017-08-23 13:03:04 +0200

    modify checktags.sh for funkytags support, and generate tags

--- checktags.sh
+++ checktags.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 res=''
 for i in `ls _posts/*.md`; do
-	res+=`grep 'tags:' $i`;
-	res+="\n";
+	funkytag=`cat $i|sed -n '/---/,/---/p'|grep '^ '`;
+	if [ $? -eq 0 ]; then
+		echo "Post $i has funky tags";
+		tagstr='tags: ['
+		tagstr+=`cat $i|sed -n '/---/,/---/p'|grep '^ '|awk '{print $2}'|paste -sd,`;
+		tagstr+=']\n';
+		res+=$tagstr
+	else
+		res+=`grep 'tags:' $i`;
+		res+="\n";
+	fi
 done
 for tag in `echo -e $res|cut -d '[' -f2|cut -d ']' -f1|tr -d '[ ]'|tr '\r\n' ',' |tr -s ','|tr ',' '\n'|sort -u`; do
 	echo "Checking for tag $tag";
 	if [ -s tag/$tag.md ]; then
+		#echo "Tag $tag exists, so skipping.";
 		continue;
 	else
 		echo "Tag $tag not found, so creating";
---
+++ tag/festivals.md
@@ -0,0 +1,5 @@
+---
+layout: default
+title: festivals
+---
+{% include tag.html %}
